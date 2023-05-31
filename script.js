25297a1dba5f7f420780102b7efa5be50e7a3596
srdizjgtrsdo4 <135130634+srdizjgtrsdo4@users.noreply.github.com>
2023-05-31 15:50:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// HOME SWIPER
+  var swiper = new Swiper("home-swiper ", {
+      spaceBetween: 85,
+      centeredSlides: true,
+     // autoplay: {
+        // delay: 2500,
+       // disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+     
+});
